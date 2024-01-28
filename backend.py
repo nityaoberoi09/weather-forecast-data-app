169dc2029d900423d554437048fc6bf1411962df
nitya <nityaoberoi09@gmail.com>
2024-01-28 22:48:30 +0530

    adding the dynamic graph for temp and adding the sky images

--- backend.py
+++ backend.py
@@ -9,10 +9,7 @@ def get_data(place, forecast_days=None, kind=None):
     filtered_data=data["list"]
     nr_value =8*forecast_days #applying unitary method
     filtered_data=filtered_data[:nr_value]
-    if kind=="Temprature":
-        filtered_data=[dict["main"]["temp"]for dict in filtered_data]
-    if kind=="Sky":
-        filtered_data=[dict["weather"][0]["main"]for dict in filtered_data]
+
     return filtered_data
 
 if __name__=="__main__":
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from backend import get_data
-
+import plotly.express as px
 
 st.title("Weather Forecast for the next days")
 place=st.text_input("place")
@@ -12,4 +12,19 @@ option=st.selectbox("select data to view",
                     ("Temprature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
-data=
+if place:
+    filtered_data=get_data(place, days)
+    if option=="Temprature":
+        tempratures=[dict["main"]["temp"]for dict in filtered_data]
+        date=[dict["dt_txt"]for dict in filtered_data]
+        #plotting map
+        figure= px.line(x=date, y=tempratures, labels={"x":"date","y":"temprature(C)"})
+        st.plotly_chart(figure)
+
+    if option=="Sky":
+        images={"Clear":"images/clear.png","Clouds":"images/cloud.png",
+               "Rain":"images/rain.png","Snow":"images/snow.png"}
+        sky_condition=[dict["weather"][0]["main"]for dict in filtered_data]
+        image_paths=[images[condition] for condition in sky_condition ]
+        print(sky_condition)
+        st.image(image_paths, width=115)
